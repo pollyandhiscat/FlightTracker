107f78238a78ced6342f488a63f15dc4f074baf7
pollyandhiscat <pollyandhiscat@outlook.com>
2026-06-12 10:31:34 -0700

    Add Models folder to track model files

---
+++ FlightTracker/Models/Flight.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FlightTracker.Models
+{
+    public class Flight
+    {
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public enum OwnershipTypes { SOLE, FRACTIONAL, CHARTER };
+
+        public int Id { get; set; }
+
+        // International Civil Aviation Organization code.
+        [StringLength(4, MinimumLength = 2)]
+        public string IcaoTypeCode { get; set; } = string.Empty;
+
+        // Tail numbers should not be lowercase and contain invalid characters like underscores.
+        [RegularExpression(@"^[A-Z0-9-]+$", ErrorMessage = "Invalid tail number format.")]
+        public string TailNumber { get; set; } = String.Empty;
+
+        public string DepartureAirport { get; set; } = String.Empty;
+        public string ArrivalAirport { get; set; } = String.Empty;
+        public string Ownership { get; set; } = String.Empty;
+        public int RunwayRequirementFeet { get; set; }
+        public DateTime DepartureTime { get; set; }
+
+    }
+
+}

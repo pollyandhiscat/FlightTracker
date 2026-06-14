9fc6457f5301850bc6e21b81ef6042cacd05a3f7
pollyandhiscat <pollyandhiscat@outlook.com>
2026-06-13 20:09:59 -0700

    Add AppDbContext to create DbContext and set the Flights table

---
+++ FlightTracker/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FlightTracker.Models;
+
+namespace FlightTracker.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Flight> Flights => Set<Flight>();
+    }
+}

e1cb73f0f7b1a87e1a3bf49f3545a21a7e3cc4c3
pollyandhiscat <pollyandhiscat@outlook.com>
2026-06-14 11:43:21 -0700

    Add CreateFlightRequest DTO to model an incoming request to create a new flight

---
+++ FlightTracker/DTO/CreateFlightRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace FlightTracker.DTO
+{
+    public class CreateFlightRequest
+    {
+        public string IcaoTypeCode { get; set; } = string.Empty;
+        public string TailNumber { get; set; } = String.Empty;
+        public string DepartureAirport { get; set; } = String.Empty;
+        public string ArrivalAirport { get; set; } = String.Empty;
+        public string Ownership { get; set; } = String.Empty;
+        public int RunwayRequirementFeet { get; set; }
+        public DateTime DepartureTime { get; set; }
+
+    }
+}

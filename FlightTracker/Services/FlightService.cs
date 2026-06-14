82394ac27d0435ac68349b7f9291f66ff7fcfb2d
pollyandhiscat <pollyandhiscat@outlook.com>
2026-06-14 11:54:59 -0700

    Implement logic for CreateFlightAsync() and UpdateFlightAsync(), including mapping new request data to appropriate DTO properties

--- FlightTracker/Services/FlightService.cs
+++ FlightTracker/Services/FlightService.cs
@@ -14,7 +14,7 @@ namespace FlightTracker.Services
             // Map the EF Core select query result to our DTO object.
             return await context.Flights.Select(f => new FlightResponse
             {
-
+                Id = f.Id,
                 IcaoTypeCode = f.IcaoTypeCode,
                 TailNumber = f.TailNumber,
                 DepartureAirport = f.DepartureAirport,
@@ -32,7 +32,7 @@ namespace FlightTracker.Services
                 .Where(f => f.Id == id)
                 .Select(f => new FlightResponse
                 {
-
+                    Id = f.Id,
                     IcaoTypeCode = f.IcaoTypeCode,
                     TailNumber = f.TailNumber,
                     DepartureAirport = f.DepartureAirport,
@@ -46,22 +46,78 @@ namespace FlightTracker.Services
             return flight;
         }
 
-        public Task<FlightResponse> AddFlightAsync(Flight flight)
+        public async Task<FlightResponse> CreateFlightAsync(CreateFlightRequest flight)
         {
+            // Map data from new flight request to a Flight model.
+            var newFlight = new Flight
+            {
 
-            throw new NotImplementedException();
+                IcaoTypeCode = flight.IcaoTypeCode,
+                TailNumber = flight.TailNumber,
+                DepartureAirport = flight.DepartureAirport,
+                ArrivalAirport = flight.ArrivalAirport,
+                Ownership = flight.Ownership,
+                RunwayRequirementFeet = flight.RunwayRequirementFeet,
+                DepartureTime = flight.DepartureTime
+
+            };
+
+            context.Flights.Add(newFlight);
+
+            // EF Core will handle Id assignment.
+            await context.SaveChangesAsync();
+
+            // Map newly created Flight model data to a response DTO.
+            return new FlightResponse
+            {
+                Id = newFlight.Id,
+                IcaoTypeCode = newFlight.IcaoTypeCode,
+                TailNumber = newFlight.TailNumber,
+                DepartureAirport = newFlight.DepartureAirport,
+                ArrivalAirport = newFlight.ArrivalAirport,
+                Ownership = newFlight.Ownership,
+                RunwayRequirementFeet = newFlight.RunwayRequirementFeet,
+                DepartureTime = newFlight.DepartureTime
+
+            };
+            
         }
 
-        public Task<bool> UpdateFlightAsync(int id)
+        public async Task<bool> UpdateFlightAsync(int id, UpdateFlightRequest flight)
         {
+            var existingFlight = await context.Flights.FindAsync(id);
+            if (existingFlight == null)
+            {
+                return false;
+            }
 
-            throw new NotImplementedException();
+            if (!string.IsNullOrWhiteSpace(flight.IcaoTypeCode)) existingFlight.IcaoTypeCode = flight.IcaoTypeCode;
+            if (!string.IsNullOrWhiteSpace(flight.TailNumber)) existingFlight.TailNumber = flight.TailNumber;
+            if (!string.IsNullOrWhiteSpace(flight.DepartureAirport)) existingFlight.DepartureAirport = flight.DepartureAirport;
+            if (!string.IsNullOrWhiteSpace(flight.ArrivalAirport)) existingFlight.ArrivalAirport = flight.ArrivalAirport;
+            if (!string.IsNullOrWhiteSpace(flight.Ownership)) existingFlight.Ownership = flight.Ownership;
+
+            existingFlight.RunwayRequirementFeet = flight.RunwayRequirementFeet ?? existingFlight.RunwayRequirementFeet;
+            existingFlight.DepartureTime = flight.DepartureTime ?? existingFlight.DepartureTime;
+
+            await context.SaveChangesAsync();
+            return true;
+
         }
 
-        public Task<bool> DeleteFlightAsync(int id)
+        public async Task<bool> DeleteFlightAsync(int id)
         {
 
-            throw new NotImplementedException();
+            var existingFlight = await context.Flights.FindAsync(id);
+            if (existingFlight == null)
+            {
+                return false;
+            }
+
+            context.Flights.Remove(existingFlight);
+            await context.SaveChangesAsync();
+            return true;
+
         }
 
     }

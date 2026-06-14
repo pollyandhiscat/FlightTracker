01a345d516750ce8ccb14c5ab5ffa62b579e1b54
pollyandhiscat <pollyandhiscat@outlook.com>
2026-06-14 09:48:34 -0700

    Update GetAllFlightsAsync() and GetFlightByIdAsync() to return a FlightResponse DTO and map returned data to that new DTO object

--- FlightTracker/Services/FlightService.cs
+++ FlightTracker/Services/FlightService.cs
@@ -1,25 +1,52 @@
 ﻿using FlightTracker.Data;
+using FlightTracker.DTO;
 using FlightTracker.Models;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace FlightTracker.Services
 {
     public class FlightService(AppDbContext context) : IFlightService
     {
-        public async Task<List<Flight>> GetAllFlightsAsync()
+        public async Task<List<FlightResponse>> GetAllFlightsAsync()
         {
+            // Map the EF Core select query result to our DTO object.
+            return await context.Flights.Select(f => new FlightResponse
+            {
 
-            return await context.Flights.ToListAsync();
+                IcaoTypeCode = f.IcaoTypeCode,
+                TailNumber = f.TailNumber,
+                DepartureAirport = f.DepartureAirport,
+                ArrivalAirport = f.ArrivalAirport,
+                Ownership = f.Ownership,
+                RunwayRequirementFeet = f.RunwayRequirementFeet,
+                DepartureTime = f.DepartureTime
+            }).ToListAsync();
 
         }
 
-        public async Task<Flight?> GetFlightByIdAsync(int id)
+        public async Task<FlightResponse?> GetFlightByIdAsync(int id)
         {
-            var flight = await context.Flights.FindAsync(id);
+            var flight = await context.Flights
+                .Where(f => f.Id == id)
+                .Select(f => new FlightResponse
+                {
+
+                    IcaoTypeCode = f.IcaoTypeCode,
+                    TailNumber = f.TailNumber,
+                    DepartureAirport = f.DepartureAirport,
+                    ArrivalAirport = f.ArrivalAirport,
+                    Ownership = f.Ownership,
+                    RunwayRequirementFeet = f.RunwayRequirementFeet,
+                    DepartureTime = f.DepartureTime
+                })
+                .FirstOrDefaultAsync();
+            
             return flight;
         }
 
-        public Task<Flight> AddFlightAsync(Flight flight)
+        public Task<FlightResponse> AddFlightAsync(Flight flight)
         {
 
             throw new NotImplementedException();

5898d5cb6c263916f9af57a24693e1478e4159cc
pollyandhiscat <pollyandhiscat@outlook.com>
2026-06-14 09:49:34 -0700

    Update GetAllFlightsAsync(), GetFlightByIdAsync(), and AddFlightAsync() to return a FlightResponse DTO

--- FlightTracker/Services/IFlightService.cs
+++ FlightTracker/Services/IFlightService.cs
@@ -1,13 +1,14 @@
-﻿using FlightTracker.Models;
+﻿using FlightTracker.DTO;
+using FlightTracker.Models;
 
 namespace FlightTracker.Services
 {
     public interface IFlightService
     {
 
-        Task<List<Flight>> GetAllFlightsAsync();
-        Task<Flight?> GetFlightByIdAsync(int id);
-        Task<Flight> AddFlightAsync(Flight flight);
+        Task<List<FlightResponse>> GetAllFlightsAsync();
+        Task<FlightResponse?> GetFlightByIdAsync(int id);
+        Task<FlightResponse> AddFlightAsync(Flight flight);
         Task<bool> UpdateFlightAsync(int id);
         Task<bool> DeleteFlightAsync(int id);
     }

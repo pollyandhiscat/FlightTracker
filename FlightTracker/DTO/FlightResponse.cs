b2c46230760a0871af21edd77f23adac1b40dd92
pollyandhiscat <pollyandhiscat@outlook.com>
2026-06-14 09:47:14 -0700

    Add DTO folder to house models for DTO objects

---
+++ FlightTracker/DTO/FlightResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace FlightTracker.DTO
+{
+    public class FlightResponse
+    {
+        public string IcaoTypeCode { get; set; } = string.Empty;
+        public string TailNumber { get; set; } = String.Empty;
+        public string DepartureAirport { get; set; } = String.Empty;
+        public string ArrivalAirport { get; set; } = String.Empty;
+        public string Ownership { get; set; } = String.Empty;
+        public int RunwayRequirementFeet { get; set; }
+        public DateTime DepartureTime { get; set; }
+
+    }
+}
